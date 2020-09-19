60f276117d484bdc198565ebf5fca5eadbab64b8
Tristan Joshua Alba <tristan.j.alba@gmail.com>
2020-09-19 15:30:20 +0800

    Adding new logic for the JS memoryGame project

--- memoryGame/js/script.js
+++ memoryGame/js/script.js
@@ -10,19 +10,50 @@
 
 //     document.body.appendChild(div);
 // }, false);
-
+// var randomNum = Math.random();
+// filtered[Math.floor(randomNum * filtered.length)].classList.add("selected-ai");
 
 var numBlocks = prompt('How many blocks?');
 
 function genBlocks(numBlocks) {
+    // Create array of id's to mark 
+    var toMemorize = [];
+    for (i = 1; i <= numBlocks; i++) {
+        if (Math.random() >= 0.50) {
+            toMemorize.push('x')
+        } else {
+            toMemorize.push('o')
+        }
+    };
+
     for (i = 1; i <= numBlocks; i++) {
         console.log(i);
 
         var newBlock = document.createElement('div');
+        newBlock.id = toMemorize[i];
         newBlock.className = "memory-blocks";
 
         document.getElementById('game-area').appendChild(newBlock);
     }
 }
 
+// Display Patterns Momentarily
+function displayPattern() {
+    var memoryBlocks = document.querySelectorAll('.memory-blocks')
+    memoryBlocks.forEach(function(a) {
+        if (a.id === 'x') {
+            a.classList.add('pattern')
+        } else {
+            null
+        }
+        setTimeout(function() {
+            a.classList.remove('pattern')
+
+        }, 3000)
+    })
+
+}
+
 genBlocks(numBlocks);
+
+displayPattern();
