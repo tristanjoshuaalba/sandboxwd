a113d33fdd9d5719ebea9ae11bbc46c2c1d57c4e
Tristan Joshua Alba <tristan.j.alba@gmail.com>
2020-10-01 22:04:35 +0800

    Adding new files

--- MyFirstReactApp/robofriends/src/components/CardList.js
+++ MyFirstReactApp/robofriends/src/components/CardList.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({robots}) =>{
-    if(true){
-        throw new Error('NOOOOOO!!!!');
-    };
+    // if(true){
+    //     throw new Error('NOOOOOO!!!!');
+    // };
     const cardComponent = robots.map((user,i) => {
         return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
     })
