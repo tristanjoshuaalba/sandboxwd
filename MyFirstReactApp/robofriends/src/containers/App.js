e5bce9406d0ff113768372870723b4eda23e5d85
Tristan Joshua Alba <tristan.j.alba@gmail.com>
2020-10-02 20:57:56 +0800

    Updating Redux

--- MyFirstReactApp/robofriends/src/containers/App.js
+++ MyFirstReactApp/robofriends/src/containers/App.js
@@ -21,6 +21,7 @@ class App extends Component {
   }
 
   componentDidMount(){
+    console.log(this.props.store.getState())
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => {
                         return response.json();
--- MyFirstReactApp/robofriends/src/index.js
+++ MyFirstReactApp/robofriends/src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {searchRobots} from './reducers';
+import {createStore} from 'redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+// import { searchRobots } from './reducers';
 
+
+const store = createStore(searchRobots)
 
 ReactDOM.render( 
-    <App/>
-    ,
-    document.getElementById('root')
+    <Provider store = {store}> 
+            <App/>  
+    </Provider>
+    , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
