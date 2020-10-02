f8b6be6b3f9ce8004b1818ef982f94b21141927a
Tristan Joshua Alba <tristan.j.alba@gmail.com>
2020-10-02 20:44:01 +0800

    Adding reducers.js

--- MyFirstReactApp/robofriends/src/actions.js
+++ MyFirstReactApp/robofriends/src/actions.js
@@ -1,6 +1,8 @@
 // Creating an Action:
 // Action: setSearchfield that takes in input: text of type: 'CHANGE_SEARCH_FIELD'
+import {CHANGE_SEARCH_FIELD} from './constants.js'
+
 export const setSearchField = (text) => ({
-    type: 'CHANGE_SEARCH_FIELD',
+    type: CHANGE_SEARCH_FIELD,
     payload: text
 })
