8c34beb7d6c56d4ebbc43797579e73dc5a6c8a6b
Tristan Joshua Alba <tristan.j.alba@gmail.com>
2020-10-29 23:18:30 +0800

    Add profile picture to sample blog site Configure layout and styling to manage global CSS attributes across pages

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import Layout from '../../components/layout';
+import App from '../_app';
 
 const FirstPost = () => {
     return (
@@ -9,12 +10,10 @@ const FirstPost = () => {
             <Head>
                 <title>First Post</title>
             </Head>
-            <h1>FIRST POST OH YEAH</h1>
-            <h2>
-                <Link href = "/">
-                    <a> Back to home </a>
-                </Link>
-            </h2>
+            <h1> The importance of continuous improvement </h1>
+            <p>
+                Life is too short to live a dull mediocre life. We all need to constantly sharpen our saws and become the best that we can be. 
+            </p>
             </Layout>
             
         </div>
